27cb173d69e5c3c987342d63fb774b7c0e04da0d
KloknA(Artem) <worknowwork@bk.ru>
2025-11-14 17:33:41 +0300

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    private Rigidbody2D rb;
+    private PlayerInput playerInput;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        
+        Vector2 velocity = rb.linearVelocity;
+        velocity.x = moveInput.x * speed;
+        velocity.y = moveInput.y * speed;
+        rb.linearVelocity = velocity;
+
+        if (moveInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveInput.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+}
